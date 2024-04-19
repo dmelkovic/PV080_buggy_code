86e969f6f04c6b6dfb0764f3ce83959f480dda3c
unknown <xmelkov1@ad.fi.muni.cz>
2024-04-19 10:45:56 +0200

    Some fixes

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -27,7 +27,7 @@ def fetch_website(urllib_version, url):
     # Import the requested version (2 or 3) of urllib
     exec(f"import urllib{urllib_version} as urllib", globals())
     # Fetch and print the requested URL
- 
+
     try: 
         http = urllib.PoolManager()
         r = http.request('GET', url)
@@ -39,11 +39,13 @@ def load_yaml(filename):
     stream = open(filename)
     deserialized_data = yaml.load(stream, Loader=yaml.Loader) #deserializing data
     return deserialized_data
-    
+
+
 def authenticate(password):
     # Assert that the password is correct
     assert password == "Iloveyou", "Invalid password!"
     print("Successfully authenticated!")
+
 
 if __name__ == '__main__':
     print("Vulnerabilities:")
@@ -64,4 +66,3 @@ if __name__ == '__main__':
     elif choice == "4":
         password = input("Enter master password: ")
         authenticate(password)
-
